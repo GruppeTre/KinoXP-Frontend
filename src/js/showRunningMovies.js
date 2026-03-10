66b3be028f0aa0379d50e354b0e9b0f2a6b8295c
Jacob Klitgaard <jacobklitgaardhansen@gmail.com>
2026-03-10 11:08:48 +0100

    running movies rendered from backend

--- src/js/showRunningMovies.js
+++ src/js/showRunningMovies.js
@@ -1,38 +1,55 @@
+import {apiRequest} from "./module/apiRequest.js";
+
 const moviesSection = document.querySelector("section")
 
-const movies = [
-    {
-        id: 1,
-        title: "The Dark Knight",
-        description: "Dark Knight description",
-        director: "Christopher Nolan",
-        premiere: "2025-05-10",
-        imgHref : "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQkUywIUXDjHSQJIaNHYVs08osgBpF5Ot-xmB_omyEZeeRP9Xug",
-        rating : 15,
-        genres : ["ACTION", "DRAMA"]
+let movies = [];
+
+
+function getGenreString(movie) {
+    return movie.genres.map(g => g.name).join(", ");
+}
+
+function renderMovies(movies) {
+
+    movies.forEach(movie => {
+        const article = document.createElement("article");
+        article.innerHTML = `
+    <h2>${movie.title}</h2>
+    <p>${movie.description}</p>
+    <p>${getGenreString(movie)}</p>
+    <p>Director: ${movie.director}</p>
+    <p>Aldersgrænse: ${movie.rating.name}</p>
+    <p>Premiere: ${new Date(movie.premiere).toLocaleDateString()}</p>
+    <a href="http://localhost:8080/movie/${movie.id}" target="_blank">
+        <img src="${movie.imgHref}" alt="${movie.title}">
+    </a>
+    `;
+        moviesSection.appendChild(article);
+    })
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+
+    try {
+        movies = await apiRequest("http://localhost:8080/movie")
+    } catch (error) {
+        console.error("failed to fetch movie data", error)
+        alert("Something went wrong. Try again later")
+        return;
     }
-];
-
-movies.push({
-    id: 2,
-    title: "The Mask",
-    description: "A shy bank clerk discovers a magical mask.",
-    director: "Chuck Russell",
-    premiere: "1994-07-29",
-    imgHref: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiR8_9_An2NlR4qpZE_eSXf0AMYLVX7G9gzRDRqWoyq8axhIhA",
-    rating: 11,
-    genres: ["COMEDY"]
-});
-
-movies.forEach(movie => {
-    const article = document.createElement("article");
-    article.innerHTML = `
-<h2>${movie.title}</h2>
-<p>${movie.description}</p>
-<p>Director: ${movie.director}</p>
-<p>Aldersgrænse: ${movie.rating}</p>
-<p>Premiere: ${new Date(movie.premiere).toLocaleDateString()}</p>
-<img src="${movie.imgHref}" alt="${movie.title}">
-`;
-    moviesSection.appendChild(article);
+    renderMovies(movies)
 })
+
+
+//     [
+//     {
+//         id: 1,
+//         title: "The Dark Knight",
+//         description: "Dark Knight description",
+//         director: "Christopher Nolan",
+//         premiere: "2025-05-10",
+//         imgHref : "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQkUywIUXDjHSQJIaNHYVs08osgBpF5Ot-xmB_omyEZeeRP9Xug",
+//         rating : 15,
+//         genres : ["ACTION", "DRAMA"]
+//     }
+// ];
